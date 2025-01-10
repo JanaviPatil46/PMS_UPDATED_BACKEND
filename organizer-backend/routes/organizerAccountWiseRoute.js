caa6ce5192d3df07df61d9df103684c0363ed3e0
Janavi Patil <janavijpatil0406@gmail.com>
2025-01-10 18:16:50 +0530

    10/01/2025

--- organizer-backend/routes/organizerAccountWiseRoute.js
+++ organizer-backend/routes/organizerAccountWiseRoute.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { createOrganizerAccountWise, getOrganizerAccountWise, getOrganizerAccountWises, updateOrganizerAccountWiseStatus,deleteOrganizerAccountWise, getOrganizerByAccountId, updateOrganizerAccountWise,getActiveOrganizerAccountWises } = require("../controller/organizerAccountWiseController");
+const { createOrganizerAccountWise, getOrganizerAccountWise, getOrganizerAccountWises, updateOrganizerAccountWiseStatus,deleteOrganizerAccountWise, getOrganizerByAccountId, updateOrganizerAccountWise,
+    // getActiveOrganizerAccountWises
+
+ } = require("../controller/organizerAccountWiseController");
 
 //******organizer Accountwise Start******** */
 
 router.get("/organizeraccountwise", getOrganizerAccountWises);
 router.get("/organizeraccountwise/:id", getOrganizerAccountWise);
-router.get("/organizeraccountwise/:isactive/:accountid",getActiveOrganizerAccountWises);
+// router.get("/organizeraccountwise/:isactive/:accountid",getActiveOrganizerAccountWises);
 router.post("/organizeraccountwise/org", createOrganizerAccountWise);
 router.delete("/organizeraccountwise/:id", deleteOrganizerAccountWise);
-router.get("/organizeraccountwise/organizerbyaccount/:id", getOrganizerByAccountId);
+// router.get("/organizeraccountwise/organizerbyaccount/:id", getOrganizerByAccountId);
+router.get("/organizeraccountwise/organizerbyaccount/accid/:id/:isactive", getOrganizerByAccountId);
+
 router.patch("/organizeraccountwise/:id", updateOrganizerAccountWise);
 router.patch("/organizeraccountwise/organizeraccountwisestatus/:id/:issubmited", updateOrganizerAccountWiseStatus);
 //******organizer Accountwise ENd******** */
